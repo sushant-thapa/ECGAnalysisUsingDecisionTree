3d2f4c6323541a10fe993c243749dfc9a9fb596a
Rajat Parajuli <073bct532.rajat@pcampus.edu.np>
2019-07-04 20:29:23 +0545

    Added some styles to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 		super(props)
 		this.state = {
 			apis : [
-			{title: "Classifies a heartbeat",
+			{title: "Classify a heartbeat",
 			 method: "POST",
 			 path: "/classifybeat",
 			 params: [
@@ -19,35 +19,103 @@ class App extends Component {
 	getApi(method,path){
 		for (let i=0; i < this.state.apis.length ; i++){
 			let api = this.state.apis[i]
-			if (api.method == method && api.path == path){
+			if (api.method === method && api.path === path){
 				return api
 			}
 		}
 	}
 
 	prepareParam(param){
-		let name,type,desc = param
-		return <div>
-			<div>{name}</div>
-			<div>{type}</div>
-			<div>{desc}</div>
+		const styles = {
+			fields: {
+				display: 'flex',
+				borderBottom : '1px solid',
+			},
+			name: {
+				width: '20%'
+			},
+			type: {
+				width: '20%'
+			},
+			desc: {
+				width: '50%'
+			}
+		}
+		let [name,type,desc] = param
+		return <div style={styles.fields}>
+			<div style={styles.name}>{name}</div>
+			<div style={styles.type}>{type}</div>
+			<div style={styles.desc}>{desc}</div>
 		</div>
 	}
 
 	prepareAPI(api){
-		return <div>
-			<div>
+		const styles = {
+			apiContainer: {
+				//border : '1px solid',
+				marginTop: '20px',
+				maxWidth: '50%',
+
+			},
+			mpContainer : {
+				display: 'flex',
+			},
+			title : {
+				fontSize: '1.5em'
+			},
+			method: {
+				color: '#ee1111',
+				fontSize: '2em',
+			},
+			path: {
+				position: 'relative',
+				marginLeft: '10%',
+				color: '#00aa00',
+				fontSize: '1.2em'
+			},
+			pathSpan: {
+				position: 'absolute',
+				bottom: '0'
+			},
+			field : {
+				borderBottom : '1px solid #000000',
+				fontSize: '1.2em',
+				color: 'blue'
+			},
+			ntdContainer: {
+				display: 'flex',
+				borderBottom : '1px solid',
+			},
+			name: {
+				width: '20%'
+			},
+			type: {
+				width: '20%'
+			},
+			desc: {
+				width: '60%'
+			}
+		}
+		return <div style={styles.apiContainer}>
+			<div style={styles.title}>
 			{api.title}
 			</div>
-			<div>
-				<div>{api.method}</div><div style={{display: 'inline-block'}}>{api.path}</div>
+			<div style={styles.mpContainer}>
+				<div style={styles.method}>{api.method}</div>
+				<div style={styles.path}><span style={styles.pathSpan}>{api.path}</span></div>
 			</div>
 			<div>
-				<div></div>
-				<div>
-					<div>Name</div><div>Type</div><div>Description</div>
+				<div style={styles.field}>
+				Fields
 				</div>
-				{api.params.map( param => this.prepareParam(param))}
+				<div style={styles.ntdContainer} >
+					<div style={styles.name}>Name</div>
+					<div style={styles.type}>Type</div>
+					<div style={styles.desc}>Description</div>
+				</div>
+				<div>
+					{api.params.map( param => this.prepareParam(param))}
+				</div>
 			</div>
 		</div>
 	}
